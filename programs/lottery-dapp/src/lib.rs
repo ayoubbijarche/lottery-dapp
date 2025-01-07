eab1c1d52fd898c0545d77429d7990cf27c36474
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-01-07 20:05:41 +0100

    added tasks & wrote some stuff !

--- programs/lottery-dapp/src/lib.rs
+++ programs/lottery-dapp/src/lib.rs
@@ -1,53 +1,161 @@
 use anchor_lang::prelude::*;
-use rand::prelude::*;
+use rand::{distributions::Alphanumeric , Rng};
+
+
+
+
+//tasks
+
+
+/*
+    - generate code [x]
+    - generates tickets [x]
+    - create init users []
+    - assign tickets to users []
+    - init timer []
+    - reward distribution []
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 declare_id!("9E3vbRyXQ5hXzoJTmDmg25jvDf2kx7MipnPQmGN59Qft");
 
 #[program]
 pub mod lottery_dapp {
-    use super::*;
 
+    use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let ticket = &mut ctx.accounts.ticket_acc;
+        ticket.price = 0.25;
+        let i = 0;
+        for i in 0..200{
+            ticket.ticket_code[i].to_string();
+        }
+        ticket.ticket_code[i] = " ".to_string();
         Ok(())
     }
+
+    pub fn generate_code(ctx : Context<Genacc>) -> Result<()>{
+        let code_gen = &mut ctx.accounts.rand_acc;
+        code_gen.code = (0..200)
+        .map(|_| {
+            rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(32)
+                .map(char::from)
+                .collect()
+        })
+        .collect();
+        Ok(())
+    }
+
+    pub fn ticket_gen(ctx : Context<Ticketacc> ,) -> Result<()>{
+        let ticket = &mut ctx.accounts.ticket_acc;
+        let rand = &mut ctx.accounts.rand_acc;
+        let i = 0;
+        for i in 0..200{
+            rand.code[i].to_string();
+        }
+        ticket.ticket_code[i] = rand.code[i].to_string();
+        ticket.price = 0.25;
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info>{
     #[account(
         init,
-        payer = signer,
+        payer = admin,
         space = Ticket::INIT_SPACE
     )]
     pub ticket_acc : Account<'info , Ticket>,
 
     #[account(
         init,
-        payer = signer,
+        payer = admin,
         space = Ticket::INIT_SPACE
     )]
-
     pub timer_acc : Account<'info , Timer>,
+    
+    #[account(
+        init,
+        payer = admin,
+        space = Randcode::INIT_SPACE
+    )]
+    pub rand_acc : Account<'info , Randcode>,
 
     #[account(mut)]
-    pub signer : Signer<'info>,
+    pub admin : Signer<'info>,
     pub system_program : Program<'info , System>
 }
+
+
+#[derive(Accounts)]
+pub struct Genacc<'info>{
+    #[account(
+        init,
+        payer = admin,
+        space = Randcode::INIT_SPACE
+    )]
+    pub rand_acc : Account<'info , Randcode>,
+    #[account(mut)]
+    pub admin : Signer<'info>,
+    pub system_program : Program<'info , System>
+}
+
+#[derive(Accounts)]
+pub struct Ticketacc<'info>{
+    #[account(
+        init,
+        payer = admin,
+        space = Ticket::INIT_SPACE
+    )]
+    pub ticket_acc : Account<'info , Ticket>,
+
+    #[account(
+        init,
+        payer = admin,
+        space = Ticket::INIT_SPACE
+    )]
+    pub rand_acc : Account<'info , Randcode>,
+    #[account(mut)]
+    pub admin : Signer<'info>,
+    pub system_program : Program<'info , System>
+}
+
+
 
 #[account]
 #[derive(InitSpace)]
 pub struct Ticket{
-    #[max_len(32)]
-    pub ticket_code : String
+    #[max_len(32 , 200)]
+    pub ticket_code : Vec<String>,
+    pub price : f32
 }
 
 #[account]
 #[derive(InitSpace)]
 pub struct Timer{
-
+    pub duration : u64
 }
 
+
+// random code account
 #[account]
 #[derive(InitSpace)]
 pub struct Randcode{
@@ -55,6 +163,8 @@ pub struct Randcode{
     pub code : Vec<String> // generated codes goes to the vector
 }
 
+
+//all participating users
 #[account]
 #[derive(InitSpace)]
 pub struct Participates{
@@ -64,6 +174,8 @@ pub struct Participates{
     pub code : String
 }
 
+
+//pool account where users money is in
 #[account]
 #[derive(InitSpace)]
 pub struct Pool{
@@ -71,9 +183,11 @@ pub struct Pool{
     pub balance : u64,
 }
 
-
+// user account data
 #[account]
 #[derive(InitSpace)]
 pub struct User{
-
+    pub key : Pubkey,
+    pub ticket : Ticket,
+    pub iswinner : bool,
 }
